a3eb7c97bc575f27167c1e7fe55cda64fdb7be46
nightskyshop1023 <nightskyshop1023@naver.com>
2022-09-19 15:08:42 +0900

    Clean Code

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -4,33 +4,9 @@ import {
     GithubAuthProvider,
     GoogleAuthProvider,
 } from "@firebase/auth";
+import AuthForm from "components/AuthForm";
 
 const Auth = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [newAccount, setNewAccount] = useState(true);
-    const [error, setError] = useState("");
-    const onChange = (event) => {
-        const { target: {name, value} } = event;
-        if (name === "email") {
-            setEmail(value);
-        } else if (name === "password") {
-            setPassword(value);
-        }
-    }
-    const onSubmit = async (event) => {
-        event.preventDefault();
-        try {
-            if (newAccount) {
-                await authService.createUserWithEmailAndPassword(email, password);
-            } else {
-                await authService.signInWithEmailAndPassword(email, password);
-            }
-        } catch(error) {
-            setError(error.message);
-        }
-    }
-    const toggleAccount = () => setNewAccount((prev) => !prev);
     const onSocialClick = async (event) => {
         const { target: {name} } = event;
         let provider;
@@ -44,27 +20,7 @@ const Auth = () => {
 
     return (
         <div>
-            <form onSubmit={onSubmit}>
-                <input
-                    name="email"
-                    type="text"
-                    placeholder="Email"
-                    required
-                    value={email}
-                    onChange={onChange}
-                />
-                <input
-                    name="password"
-                    type="password"
-                    placeholder="Password"
-                    required
-                    value={password}
-                    onChange={onChange}
-                />
-                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
-                {error}
-            </form>
-            <span onClick={toggleAccount}>{newAccount ? "Sign in" : "Create Account"}</span>
+            <AuthForm />
             <div>
                 <button onClick={onSocialClick} name="google">Continue with Google</button>
                 <button onClick={onSocialClick} name="github">Continue with Github</button>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,10 @@
 import Hweet from "components/Hweet";
-import { v4 as uuidv4 } from "uuid";
-import { dbService, storageService } from "fbase";
-import { ref, uploadString, getDownloadURL } from "@firebase/storage";
+import { dbService } from "fbase";
 import React, { useEffect, useState } from "react";
+import HweetFactory from "components/HweetFactory";
 
 const Home = ({ userObj }) => {
-    const [hweet, setHweet] = useState("");
     const [hweets, setHweets] = useState([]);
-    const [attachment, setAttachment] = useState("");
     const getNweet = async () => {
         const dbHweets = await dbService.collection("hweets").get();
         dbHweets.forEach((document) => {
@@ -28,59 +25,9 @@ const Home = ({ userObj }) => {
             setHweets(hweetArray)
         })
     }, []);
-
-    const onSubmit = async (event) => {
-        event.preventDefault();
-        let attachmentUrl = "";
-        if (attachment !== "") {
-            const attachmentRef = ref(storageService, `${userObj.uid}/${uuidv4()}`);
-            const response = await uploadString(attachmentRef, attachment, "data_url");
-            attachmentUrl =  await getDownloadURL(response.ref);
-        } 
-        const hweetObj = {
-            text:hweet,
-            createAt: Date.now(),
-            creatorId: userObj.uid,
-            attachmentUrl
-        }
-        await dbService.collection("hweets").add(hweetObj);
-        setHweet("");
-        setAttachment("");
-    }
-    const onChange = (event) => {
-        const { target: {value} } = event;
-        setHweet(value);
-    }
-    const onFileChange = (event) => {
-        const { target: {files} } = event;
-        const theFile = files[0];
-        const reader = new FileReader();
-        reader.onloadend = (finishedEvent) => {
-            const { currentTarget: {result} } = finishedEvent;
-            setAttachment(result);
-        }
-        reader.readAsDataURL(theFile);
-    }
-    const onClearAttachment = () => setAttachment("");
     return (
         <div>
-            <form onSubmit={onSubmit}>
-                <input
-                    value={hweet}
-                    onChange={onChange}
-                    type="text"
-                    placeholder="What's on your mind?"
-                    maxLength={120}
-                />
-                <input type="file" accept="image/*" onChange={onFileChange} />
-                {attachment && (
-                    <div>
-                        <img src={attachment} width="50px" height="50px" alt="" />
-                        <button onClick={onClearAttachment}>Clear</button>
-                    </div>
-                )}
-                <input onClick={onSubmit} type="submit" value="Hweet" />
-            </form>
+            <HweetFactory userObj={userObj} />
             <div>
                 {hweets.map((hweet) => (
                     <Hweet key={hweet.id} hweetObj={hweet} isOwner={hweet.creatorId === userObj.uid} />
